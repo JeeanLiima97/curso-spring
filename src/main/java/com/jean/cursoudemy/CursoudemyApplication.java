55cf7d0a743c4c5264e7005c933751acb10eae7b
Jean Lima de Souza Junior <jean.jr.souza@hotmail.com>
2020-05-12 15:33:21 -0300

    Criação do cliente, tipo cliente e endereço

--- src/main/java/com/jean/cursoudemy/CursoudemyApplication.java
+++ src/main/java/com/jean/cursoudemy/CursoudemyApplication.java
@@ -42,16 +42,6 @@ public class CursoudemyApplication implements CommandLineRunner {
 		Produto p3 = new Produto(null, "Mouse", 20.00);
 		
 	
-		Estado est1 = new Estado(null, "Minas Gerais");
-		Estado est2 = new Estado(null, "São Paulo");
-		
-		Cidade c1 = new Cidade(null, "Uberlandia", est1);
-		Cidade c2 = new Cidade(null, "Campinas", est2);
-		Cidade c3 = new Cidade(null, "São Paulo", est2);
-		
-		est1.getCidades().addAll(Arrays.asList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2,c3));
-		
 		
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
@@ -63,6 +53,21 @@ public class CursoudemyApplication implements CommandLineRunner {
 		
 		categoriarepository.saveAll(Arrays.asList(cat1,cat2));
 		produtorepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "Campinas", est2);
+		Cidade c3 = new Cidade(null, "São Paulo", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		
+		
+		
 		estadorepository.saveAll(Arrays.asList(est1,est2));
 		cidaderepository.saveAll(Arrays.asList(c1,c2,c3));
 	
